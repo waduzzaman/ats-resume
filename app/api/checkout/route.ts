0e142a9a0a5a66d2f8b15f8348e24bcf6479873c
Waduzzaman <waduzzaman@gmail.com>
2025-12-25 16:30:56 -0500

    type error fixed

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-12-15" as any,
+  apiVersion: "2023-10-16",
 });
 
 export async function POST() {
