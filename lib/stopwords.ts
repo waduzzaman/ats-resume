a55f2a93a2b7d3faf707209553eb1a5c439a80ea
Waduzzaman <waduzzaman@gmail.com>
2026-01-06 14:50:40 -0500

    stopwords updated

--- lib/stopwords.ts
+++ lib/stopwords.ts
@@ -1,5 +1,26 @@
+// export const STOPWORDS = new Set([
+//   "the", "and", "a", "an", "to", "of", "in", "for", "on", "with",
+//   "is", "are", "was", "were", "be", "as", "by", "that", "this",
+//   "from", "or", "at", "it", "your", "you", "we", "our",
+// ]);
+
+
 export const STOPWORDS = new Set([
+  // Basic English (Your original list)
   "the", "and", "a", "an", "to", "of", "in", "for", "on", "with",
   "is", "are", "was", "were", "be", "as", "by", "that", "this",
   "from", "or", "at", "it", "your", "you", "we", "our",
+
+  // Pronouns & Possessives
+  "i", "me", "my", "mine", "their", "them", "they", "its",
+
+  // Common Resume/JD Verbs (High frequency, low value)
+  "using", "used", "working", "works", "including", "includes", 
+  "responsible", "requirements", "required", "preferred", "duties",
+  "must", "will", "can", "should", "plus", "years", "experience",
+
+  // Generic Business "Fluff"
+  "excellent", "strong", "written", "verbal", "communication", 
+  "skills", "ability", "team", "environment", "fast-paced",
+  "highly", "motivated", "successful", "proven", "demonstrated"
 ]);
